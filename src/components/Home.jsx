75accb5c2dfc125a4f2d30602450bd4a802ad6b3
macbook <macbook@PC192.168.1.141>
2021-04-09 19:11:18 -0500

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="description">Hola</div>
+            <div className="sound-sliders">Ruidos</div>
+        </div>
+    )
+}
+
+export default Home
